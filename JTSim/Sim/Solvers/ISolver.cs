40a07b3b3b3e89aa18a92d473685481ce7dc9c4b
Jaroslaw Tyma <tyma.jaroslaw@gmail.com>
2016-11-26 13:07:35 +0100

    usunięto starego sima

--- JTSim/Sim/Solvers/ISolver.cs
+++
@@ -1,9 +0,0 @@
-﻿using VectorF = MathNet.Numerics.LinearAlgebra.Vector<float>;
-
-namespace JTSim
-{
-    public interface ISolver
-    {
-        VectorF Solve(ContinousModel mdl, VectorF state, float u, float t, float h);
-    }
-}
